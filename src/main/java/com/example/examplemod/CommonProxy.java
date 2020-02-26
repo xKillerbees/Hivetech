2143e919971817b8ffcc2a84a8fa27858718acac
Robert M <robert@hivehosted.com>
2020-02-26 01:52:27 -0600

    First Setup
    Still need to add texture and recipe

---
+++ src/main/java/com/example/examplemod/CommonProxy.java
@@ -0,0 +1,16 @@
+package com.example.examplemod;
+
+import net.minecraft.item.Item;
+import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+
+@Mod.EventBusSubscriber
+public class CommonProxy {
+
+	@SubscribeEvent
+	public static void registerItems(RegistryEvent.Register<Item> event) {
+			event.getRegistry().registerAll(ExampleMod.mySword);
+	}
+	
+}
---
+++ src/main/java/com/example/examplemod/ExampleMod.java
@@ -0,0 +1,44 @@
+package com.example.examplemod;
+
+import net.minecraft.init.Blocks;
+import net.minecraft.item.Item;
+import net.minecraft.item.Item.ToolMaterial;
+import net.minecraftforge.common.util.EnumHelper;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import org.apache.logging.log4j.Logger;
+
+@Mod(modid = ExampleMod.MODID, name = ExampleMod.NAME, version = ExampleMod.VERSION)
+public class ExampleMod
+{
+    public static final String MODID = "hivetech";
+    public static final String NAME = "Hivetech";
+    public static final String VERSION = "1.0";
+
+    private static Logger logger;
+    
+    //New Variables from guide
+    public static ToolMaterial myToolMaterial;
+    public static Item mySword;
+
+    @EventHandler
+    public void preInit(FMLPreInitializationEvent event)
+    {
+        logger = event.getModLog();
+        
+        myToolMaterial = EnumHelper.addToolMaterial("MATERIAL NAME", 4, 10000, 20.0F, 100.0F, 30);
+        // - (Name, harvest level is which materials it can get resources from 4 is diamond pickaxe, max uses is obvious, how much damage to entity, how easy to enchant)
+        
+        mySword = new CustomSword();
+        
+    }
+
+    @EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+        // some example code
+        logger.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
+    }
+}
