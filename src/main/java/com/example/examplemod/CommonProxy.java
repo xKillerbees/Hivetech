380f17c6998e75fe2318b09a7ffb108c0810a823
Robert M <robert@hivehosted.com>
2020-02-26 02:00:59 -0600

    Texture Code
    Now to add actual texture

--- src/main/java/com/example/examplemod/CommonProxy.java
+++ src/main/java/com/example/examplemod/CommonProxy.java
@@ -1,6 +1,9 @@
 package com.example.examplemod;
 
+import net.minecraft.client.renderer.block.model.ModelResourceLocation;
 import net.minecraft.item.Item;
+import net.minecraftforge.client.event.ModelRegistryEvent;
+import net.minecraftforge.client.model.ModelLoader;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
@@ -8,6 +11,15 @@ import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 @Mod.EventBusSubscriber
 public class CommonProxy {
 
+	@SubscribeEvent
+	public static void registerRenders(ModelRegistryEvent event) {
+		registerRender(ExampleMod.mySword);
+	}
+	
+	private static void registerRender(Item item) {
+		ModelLoader.setCustomModelResourceLocation(item, 0, new ModelResourceLocation( item.getRegistryName(), "inventory"));
+	}
+	
 	@SubscribeEvent
 	public static void registerItems(RegistryEvent.Register<Item> event) {
 			event.getRegistry().registerAll(ExampleMod.mySword);
